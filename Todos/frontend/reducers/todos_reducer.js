e91cbb23183901d2d6c25bb11f32308fc4dd9c09
jlshaw117 <jlshaw117@gmail.com>
2018-12-03 15:16:41 -0800

    debugg reducer

--- Todos/frontend/reducers/todos_reducer.js
+++ Todos/frontend/reducers/todos_reducer.js
@@ -1,10 +1,33 @@
-import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
+import { RECEIVE_TODOS, RECEIVE_TODO, receiveTodos, receiveTodo } from '../actions/todo_actions';
+import merge from 'lodash/merge';
 
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
 
-const todosReducer = (state = {}, action) => {
+const todosReducer = (state = initialState, action) => {
   switch(action.type) {
-    // case RECEIVE_TODOS:
-      
+    case RECEIVE_TODOS:
+      let object = {};
+      action.todos.forEach((el) => {
+        object[el.id] = el;
+      });
+      return object;
+    case RECEIVE_TODO:
+      let nextState = merge({}, state);
+      nextState[action.todo.id] = action.todo;
+      return nextState;
     default:
       return state;
   }
--- Todos/frontend/todo_redux.jsx
+++ Todos/frontend/todo_redux.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
@@ -8,7 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
   window.store = store;
   ReactDOM.render(<Root />, root);
 });
-
+window.receiveTodo = receiveTodo;
+window.receiveTodos = receiveTodos;
 function Root() {
   return <h1>Todos App</h1>;
 }
