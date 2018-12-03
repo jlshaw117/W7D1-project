b707e173c4753fab34c86ffd3bae1689f2866e89
jlshaw117 <jlshaw117@gmail.com>
2018-12-03 13:51:53 -0800

    Setup directory for redux/react ToDo app:

---
+++ Todos/frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('content');
+  
+  return ReactDOM.render(<h1>Todos App</h1>, root);
+});
+
