b4b3554239acdb6766e611b3cfa5fe386b105ddd
jlshaw117 <jlshaw117@gmail.com>
2018-12-03 14:23:34 -0800

    Setup reducers

---
+++ Todos/frontend/reducers/todos_reducer.js
@@ -0,0 +1,12 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions'
+
+const todosReducer = (state = {}, action) => {
+  switch(action.type) {
+    // case RECEIVE_TODOS:
+      
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
